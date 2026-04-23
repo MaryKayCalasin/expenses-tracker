71f0f78c1d3d521409d9b01b336b63e4f2b27c43
Mary Kay Calasin <khayie27@gmail.com>
2026-04-23 14:25:52 +0200

    auto create database on startup

--- Program.cs
+++ Program.cs
@@ -37,4 +37,12 @@ app.UseSwaggerUI();
 app.UseCors();
 app.UseHttpsRedirection();
 app.MapControllers();
+
+// Auto-create database tables on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 app.Run();
