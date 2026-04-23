aa0a0e204d1ec8f05e1ec4f7c60c565c7166eb72
Mary Kay Calasin <khayie27@gmail.com>
2026-04-23 12:38:19 +0200

    add CORS for frontend

--- Program.cs
+++ Program.cs
@@ -10,11 +10,21 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=expenses.db"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseCors();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
